d4dede699ff33dcf9b3e91c7429c608068b2f3df
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-18 14:58:34 +0200

    Layout en Jetpack Compose:  BOX.

--- app/src/main/java/com/example/mynewcompose/MainActivity.kt
+++ app/src/main/java/com/example/mynewcompose/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.mynewcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mynewcompose.ui.theme.MyNewComposeTheme
@@ -28,37 +30,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MysuperText("")
+                    MyBox("")
                 }
             }
         }
     }
 }
 
-
-// We can have more Previews
-@Preview(
-    name = "PREVIEW 1!",
-    heightDp = 50,
-    widthDp = 200,
-    showBackground = true,
-    showSystemUi = true,
-    apiLevel = 29,
-    device = Devices.NEXUS_5
-
-)
-
 @Composable
-fun MyTestSuperText() {
-    MysuperText("Moreno")
+fun MyBox(name: String) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)
+                .background(androidx.compose.ui.graphics.Color.Blue)
+                .verticalScroll(rememberScrollState()), contentAlignment = Alignment.Center
+        ) {
+            Text("Example to see the box")
+        }
+    }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MysuperText(name: String) {
+fun DefaultPreview() {
     MyNewComposeTheme {
-        Text(text = "Hello Word!! \nSoy $name", modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp)
-            .clickable { })
+        MyBox("")
     }
 }
