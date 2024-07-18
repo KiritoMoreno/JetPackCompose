1efbe9b446cdbbee8883bbe7cb1297e17b40c396
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-18 14:00:14 +0200

    We put the fun outside, just to see if is working.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mynewcompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mynewcompose"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
