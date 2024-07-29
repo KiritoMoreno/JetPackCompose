277d855d780c54a04e30b158297792bd202b5fde
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-29 15:08:34 +0200

    Confirmation Dialog using forEach for the radioGroup.

--- app/src/main/java/com/example/mynewcompose/ui/theme/Dialogs.kt
+++ app/src/main/java/com/example/mynewcompose/ui/theme/Dialogs.kt
@@ -7,19 +7,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.example.mynewcompose.MyDivider
 import com.example.mynewcompose.R
 
 
@@ -84,9 +94,9 @@ fun MyCustomDialog(show: Boolean, onDismiss: () -> Unit) {
                     .fillMaxWidth()
             ) {
                 MyTitleDialog(text = "Set backup account")
-                AccountItem("Example1@gmail.com", R.drawable.avatar )
-                AccountItem("Example2@gmail.com", R.drawable.avatar )
-                AccountItem("Add account", R.drawable.add )
+                AccountItem("Example1@gmail.com", R.drawable.avatar)
+                AccountItem("Example2@gmail.com", R.drawable.avatar)
+                AccountItem("Add account", R.drawable.add)
 
             }
 
@@ -107,19 +117,76 @@ fun AccountItem(email: String, @DrawableRes drawable: Int) {
                 .size(40.dp)
                 .clip(CircleShape)
         )
-        TextButton(onClick = { Toast.makeText(context, email, Toast.LENGTH_SHORT).show()}) {
-            Text(text = email, fontSize = 14.sp, color = Color.Gray, modifier = Modifier.padding(8.dp))
+        TextButton(onClick = { Toast.makeText(context, email, Toast.LENGTH_SHORT).show() }) {
+            Text(
+                text = email,
+                fontSize = 14.sp,
+                color = Color.Gray,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+@Composable
+fun MyConfirmationDialog(show: Boolean, onDismiss: () -> Unit) {
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .background(Color.White)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                MyTitleDialog(text = "Phone ringtone", modifier = Modifier.padding(24.dp))
+                Divider(Modifier.fillMaxWidth())
+
+                var selected by remember { mutableStateOf("None") }
+
+                OptionItem(selected = selected, onItemSelected = { selected = it })
+
+                Divider(Modifier.fillMaxWidth())
+                Row(
+                    Modifier
+                        .align(Alignment.End)
+                        .padding(8.dp)
+                ) {
+                    TextButton(onClick = { onDismiss() }) {
+                        Text(text = "CANCEL")
+                    }
+                    TextButton(onClick = {
+                        // Handle the send action here
+                        onDismiss()
+                    }) {
+                        Text(text = "SEND")
+                    }
+                }
+            }
         }
     }
 }
 
+@Composable
+fun OptionItem(selected: String, onItemSelected: (String) -> Unit) {
+    Column(Modifier.fillMaxWidth()) {
+        val options = listOf("None", "Callisto", "Ganymede", "Luna")
+        options.forEach { option ->
+            Row(Modifier.padding(6.dp)) {
+                RadioButton(
+                    selected = selected == option,
+                    onClick = { onItemSelected(option) }
+                )
+                Text(text = option)
+            }
+        }
+    }
+}
 
 @Composable
-fun MyTitleDialog(text: String) {
+fun MyTitleDialog(text: String, modifier: Modifier = Modifier.padding(bottom = 12.dp)) {
     Text(
         text = text,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
-        modifier = Modifier.padding(bottom = 12.dp)
+        modifier = modifier
     )
 }
