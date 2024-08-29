7ca1c3ba12849ab0a61b4bdf3a5e5512504410fb
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-08-29 16:30:54 +0200

    Add Response and modify Data.

---
+++ app/src/main/java/com/example/mynewcompose/instagram/Login/data/network/LoginClient.kt
@@ -0,0 +1,11 @@
+package com.example.mynewcompose.instagram.Login.data.network
+
+import com.example.mynewcompose.instagram.Login.data.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface LoginClient {
+
+    @GET("/v3/f78c9d33-28b1-4f81-9cf1-6d6ff78fa014")
+    suspend fun doLogin(email:String, password:String): Response<LoginResponse>
+}
---
+++ app/src/main/java/com/example/mynewcompose/instagram/Login/data/response/LoginResponse.kt
@@ -0,0 +1,6 @@
+package com.example.mynewcompose.instagram.Login.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse (@SerializedName("success") val success:Boolean){
+}
